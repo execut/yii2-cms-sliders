d104757a339d71035ff4834485a499bb9050224b
Ruben Heymans <ruben.heymans@gmail.com>
2015-06-01 16:04:32 +0200

    Update slider widget view

--- widgets/Slider.php
+++ widgets/Slider.php
@@ -4,21 +4,40 @@ namespace infoweb\sliders\widgets;
 use Yii;
 use yii\bootstrap\Widget;
 use infoweb\sliders\models\Slider as SliderItem;
+use yii\helpers\Html;
 
 class Slider extends Widget
 {
     public $template = 'slider';
+    public $controls = '';
     public $id = 0;
     
     public function init()
     {
         parent::init();
+
+        if ($this->controls == null) {
+            $this->controls = [
+                '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span>',
+                '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span>'
+            ];
+        }
     }
     
     public function run()
     {
         $slider = SliderItem::findOne($this->id);
 
-        return $this->render($this->template, ['images' => $slider->getImages(), 'slider' => $slider]);
+        $items = [];
+
+        foreach ($slider->getImages() as $image) {
+            $items[] = [
+                'content' => Html::img($image->getUrl("{$slider->width}x{$slider->height}"), ['alt' => $image->alt, 'title' => $image->title]),
+                'caption' => $image->title,
+                'options' => [],
+            ];
+        }
+
+        return $this->render($this->template, ['items' => $items, 'controls' => $this->controls]);
     }
 }
--- widgets/views/slider.php
+++ widgets/views/slider.php
@@ -1,34 +1,5 @@
-<?php
-use yii\helpers\Html;
-?>
 
-<div id="carousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <?php foreach ($images as $k => $image) : ?>
-            <li data-target="#carousel" data-slide-to="<?= $k ?>" <?= ($k == 0) ? 'class="active"' : '' ?>></li>
-        <?php endforeach; ?>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-        <?php foreach ($images as $k => $image) : ?>
-            <div class="item <?= ($k == 0) ? 'active' : '' ?>">
-                <?= Html::img($image->getUrl("{$slider->width}x{$slider->height}"), ['class' => 'img-responsive pull-left', 'alt' => $image->alt, 'title' => $image->title]) ?>
-                <div class="carousel-caption">
-                    <?= $image->title ?>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
+<?= \yii\bootstrap\Carousel::widget([
+    'items' => $items,
+    'controls' => $controls,
+]); ?>
