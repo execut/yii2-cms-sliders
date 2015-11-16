63124f2a6376f332478ed9980fcfa8f3d526ea22
Fabio Maggio <fabiomaggio@gmail.com>
2015-11-16 16:20:43 +0100

    Remove unused attribute

--- Module.php
+++ Module.php
@@ -8,38 +8,36 @@ class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'infoweb\sliders\controllers';
 
-    public $options = [];
-    
     /**
      * The default with of a slider
      * @var int
      */
     public $defaultWith = 800;
-    
+
     /**
      * The default height of a slider
      * @var int
      */
     public $defaultHeight = 200;
-    
+
     /**
      * Enable the 'title' attribute for image's
      * @var boolean
      */
     public $enableImageTitle = false;
-    
+
     /**
      * Enable the 'subtitle' attribute for image's
      * @var boolean
      */
     public $enableImageSubTitle = false;
-    
+
     /**
      * Enable the 'description' attribute for image's
      * @var boolean
      */
     public $enableImageDescription = false;
-    
+
     /**
      * Enable the 'url' attribute for image's
      * @var boolean
